219a0450bde69ae99d56de1a24b702c5cefa8b6a
Admin <Admin@DESKTOP-PHKI08E>
2022-03-30 14:42:13 +0900

    Добавьте файлы проекта.

---
+++ ArgunovaADataSet.cs
@@ -0,0 +1,20 @@
+﻿namespace ArgunovaMaterial
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    public partial class ArgunovaADataSet
+    {
+        private static ArgunovaADataSet_Argunova;
+        public ArgunovaADataSet()
+            : base ("name=ArgunovaADataSet")
+        {
+        }
+        public static ArgunovaADataSet GetArgunova()
+        {
+            if (_context == null)
+        }
+
+           
+    }
+}
